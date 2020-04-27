6249f28c605338be4706d0689cbdae2b2415247f
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-04-27 13:40:21 -0300

    setting homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
-	before_action :authenticate_user!
+	  before_action :authenticate_user!
   	include Pundit
  
   	rescue_from Pundit::NotAuthorizedError do
-	  respond_to do |format|
-  		format.html { redirect_to root_path, flash: { error: 'Unauthorized' } }
-	  end
+  	  respond_to do |format|
+    		format.html { redirect_to root_path, flash: { error: 'Unauthorized' } }
+  	  end
+  	end
+
+  	def after_sign_in_path_for(resource)
+    	portfolios_path
   	end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
-	
-	def home
-	end
+  skip_before_action :authenticate_user!
+  before_action :is_user_logged_in?
+ 
+  def home
+  end
+ 
+  private
+ 
+  def is_user_logged_in?
+    if current_user
+      redirect_to portfolios_path
+    end
+  end
 end
