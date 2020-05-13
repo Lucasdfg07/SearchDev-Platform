226d51098790bb405ce69ed9048b1675e048d075
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-05-13 19:16:51 -0300

    setting profile block

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,4 +5,9 @@ class Profile < ApplicationRecord
  
   validates :title, :name, presence: true
   validates :photo, file: { max_size_in_bytes: 3000, mime_types: [:jpg, :png] }
+
+	def photo_url
+		return "/assets/avatars/#{self.portfolio.temp_avatar}.png" unless self.photo.attached?
+		Rails.application.routes.url_helpers.rails_blob_path(self.photo, only_path: true)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'pages#home'
   devise_for :users
  
   resources :portfolios, except: :new do
     resources :tags, only: [:create, :destroy], param: :tag_id, controller: :portfolio_tags
     resources :blocks, only: [:index, :create, :destroy]
+    resources :profiles, only: [:index, :update]
   end
  
   resources :tags, only: :index
